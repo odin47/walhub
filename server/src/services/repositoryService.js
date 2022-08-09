3c83097eab0db20501e3663e275fe71f493c6dab
Deepak <deepak@nitaras-MacBook-Pro.local>
2022-08-09 11:19:09 +0530

    modified repository details contract

--- server/src/services/repositoryService.js
+++ server/src/services/repositoryService.js
@@ -10,6 +10,7 @@ import {
 const TOP_REPOSITORY_GITHUB_URL = `${GITHUB_API_URL}/search/repositories?q=stars:>500&sort=stars&per_page=${PER_PAGE_RESULTS}`
 const REPOSITORY_LIST_URL = `${GITHUB_API_URL}/search/repositories`
 const REPOSITORY_DETAILS_URL = `${GITHUB_API_URL}/repos`
+const repoDetailsList = ['name', 'full_name', 'description', 'clone_url', 'forks', 'open_issues', 'subscribers_count']
 
 const getTopRepositories = async (currentPage = CURRENT_PAGE) => {
     const rawResult = await axios.get(`${TOP_REPOSITORY_GITHUB_URL}&page=${currentPage}`)
@@ -42,8 +43,19 @@ const getRepositoriesList = async (searchKeyword = DEFAULT_SEARCH_KEYWORD) => {
 }
 
 const getRepositoryDetails = async (repoName = DEFAULT_REPO_NAME) => {
-    const rawResult = await axios.get(`${REPOSITORY_DETAILS_URL}/${repoName}`) 
-    return rawResult.data
+    const rawResult = await axios.get(`${REPOSITORY_DETAILS_URL}/${repoName}`)
+    const filteredResult = {
+        avatarUrl:'',
+        description: []
+    }
+    repoDetailsList.forEach(item => {
+        filteredResult.description.push({
+            name: item,
+            value: rawResult.data[item]
+        })
+    })
+    filteredResult.avatarUrl = rawResult.data.owner.avatar_url
+    return filteredResult
 }
 
 export {
