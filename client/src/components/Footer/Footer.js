78a3624f4558e46d27139b9bc90dd87432425c74
Deepak <deepak@nitaras-MacBook-Pro.local>
2022-08-09 09:19:06 +0530

    Added footer details

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,9 @@
-import React from 'react'
 import styles from './Footer.module.scss'
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-
+      <p>Made with <span>❤</span> by Deepak Podili Devendra</p>
     </footer>
   )
 }
